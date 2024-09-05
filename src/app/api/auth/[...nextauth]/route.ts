869200917c0f3ec8d8a143790853b909263faaae
Mrityunjay1 <singhmrityunjay32@gmail.com>
2024-09-05 22:27:05 +0530

    fix:next-auth-1

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -47,51 +47,7 @@
 
 // export { handler as GET, handler as POST };
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { loginUser } from "../../../../../lib/actions/user.actions";
-import type { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (credentials?.email && credentials?.password) {
-          const user = await loginUser(credentials.email, credentials.password);
-          if (user) {
-            return { id: user.id, email: user.email, name: user.name };
-          }
-        }
-        return null;
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }: any) {
-      if (token) {
-        session.id = token.id;
-      }
-      return session;
-    },
-  },
-  pages: {
-    signIn: "/auth/sign-in",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "../../../../../lib/configs/auth/authOptions";
 
 const handler = NextAuth(authOptions);
 
